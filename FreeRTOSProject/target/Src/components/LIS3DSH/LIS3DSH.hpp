fdd325b74bffe85d78cb066e7af8c034cd7e81f9
Terry Louwers <terry.louwers@fourtress.nl>
2021-12-18 12:23:27 +0100

    Add initial code to start the FreeRTOS example project.

---
+++ FreeRTOSProject/target/Src/components/LIS3DSH/LIS3DSH.hpp
@@ -0,0 +1,169 @@
+/**
+ * \file    LIS3DSH.hpp
+ *
+ * \licence "THE BEER-WARE LICENSE" (Revision 42):
+ *          <terry.louwers@fourtress.nl> wrote this file. As long as you retain
+ *          this notice you can do whatever you want with this stuff. If we
+ *          meet some day, and you think this stuff is worth it, you can buy me
+ *          a beer in return.
+ *                                                                Terry Louwers
+ * \class   LIS3DSH
+ *
+ * \brief   Driver for the LIS3DSH accelerometer.
+ *
+ * \note    https://github.com/tlouwers/STM32F4-DISCOVERY/tree/develop/Drivers/components/LIS3DSH
+ *
+ * \author  T. Louwers <terry.louwers@fourtress.nl>
+ * \version 1.0
+ * \date    10-2019
+ */
+
+#ifndef LIS3DSH_HPP_
+#define LIS3DSH_HPP_
+
+/************************************************************************/
+/* Includes                                                             */
+/************************************************************************/
+#include <cstdint>
+#include <functional>
+#include "interfaces/IInitable.hpp"
+#include "interfaces/ISPI.hpp"
+#include "drivers/Pin/Pin.hpp"
+
+
+/************************************************************************/
+/* Class declaration                                                    */
+/************************************************************************/
+class LIS3DSH final : public IConfigInitable
+{
+public:
+    /**
+     * \enum    SampleFrequency
+     * \brief   Sample frequency for accelerometer data.
+     * \note    AN3393 - LIS3DSH - Application note - Table 2: Power consumption.
+     */
+    enum class SampleFrequency : uint8_t
+    {
+        _3_125_Hz,          // Low power
+        _6_25_Hz,
+        _12_5_Hz,
+        _25_Hz,             // Normal
+        _50_Hz,
+        _100_Hz,            // High performance
+        _400_Hz,
+        _800_Hz,
+        _1600_Hz
+    };
+
+    /**
+     * \enum    Scale
+     * \brief   Scale of the accelerometer data.
+     * \note    AN3393 - LIS3DSH - Application note - Table 30: Control register 5 description.
+     */
+    enum class Scale : uint8_t
+    {
+        _2_G,               ///< Default
+        _4_G,
+        _6_G,
+        _8_G,
+        _16_G,
+    };
+
+    /**
+     * \enum    AntiAliasingFilter
+     * \brief   Anti-aliasing filter bandwidth.
+     * \note    AN3393 - LIS3DSH - Application note - Table 30: Control register 5 description.
+     */
+    enum class AntiAliasingFilter : uint8_t
+    {
+        _800_Hz,            ///< Default
+        _200_Hz,
+        _400_Hz,
+        _50_Hz
+    };
+
+    /**
+     * \enum    FifoMode
+     * \brief   FIFO mode selection.
+     * \note    AN3393 - LIS3DSH - Application note - Table 37: FIFO_CTRL description.
+     */
+    enum class FifoMode : uint8_t
+    {
+        Bypass,
+        Fifo,               ///< Default
+        Stream,
+        StreamThenFifo,
+        BypassThenStream,
+        BypassThenFifo
+    };
+
+    /**
+     * \struct  Config
+     * \brief   Configuration struct for LIS3DSH.
+     */
+    struct Config : public IConfig
+    {
+        /**
+         * \brief   Constructor of the LIS3DSH configuration struct.
+         * \param   sampleFrequency     Sample frequency for accelerometer data.
+         * \param   scale               Scale of the accelerometer data. Default +/- 2G.
+         * \param   antiAliasingFilter  Anti-aliasing filter bandwidth. Default 200 Hz.
+         *
+         */
+        explicit Config(SampleFrequency sampleFrequency,
+                        Scale scale = Scale::_2_G,
+                        AntiAliasingFilter antiAliasingFilter = AntiAliasingFilter::_200_Hz) :
+            mSampleFrequency(sampleFrequency),
+            mScale(scale),
+            mAntiAliasingFilter(antiAliasingFilter)
+        { }
+
+        SampleFrequency    mSampleFrequency;        ///< Sample frequency for accelerometer data.
+        Scale              mScale;                  ///< Scale of the accelerometer data.
+        AntiAliasingFilter mAntiAliasingFilter;     ///< Anti-aliasing filter bandwidth.
+    };
+
+    LIS3DSH(ISPI& spi, PinIdPort chipSelect, PinIdPort motionInt1, PinIdPort motionInt2);
+    virtual ~LIS3DSH();
+
+    bool Init(const IConfig& config) override;
+    bool IsInit() const override;
+    bool Sleep() override;
+
+    bool Enable();
+    bool Disable();
+
+    void SetHandler(const std::function<void(uint8_t length)>& handler);
+    bool RetrieveAxesData(uint8_t* dest, uint8_t length);
+
+private:
+    ISPI&    mSpi;
+    Pin      mChipSelect;
+    Pin      mMotionInt1;
+    Pin      mMotionInt2;
+    bool     mInitialized;
+    uint8_t* mReadBuffer;
+    uint8_t  mODR;
+
+    std::function<void(uint8_t length)> mHandler;
+
+    bool SelfTest();
+    bool Configure(const IConfig& config);
+    bool PrepareReadBuffer(SampleFrequency sampleFrequency);
+    bool ClearFifo();
+    uint8_t GetSampleFrequencyAsODR(SampleFrequency sampleFrequency);
+    uint8_t GetScaleAsFCALE(Scale scale);
+    uint8_t GetAntiAliasingFilterAsBW(AntiAliasingFilter antiAliasingFilter);
+    uint8_t GetFifoModeAsFMODE(FifoMode fifoMode);
+
+    void ReadAxesCompleted();
+
+    bool WriteRegister(uint8_t reg, const uint8_t* src, uint16_t length);
+    bool ReadRegister(uint8_t reg, uint8_t* dest, uint16_t length);
+
+    void CallbackInt1();
+    void CallbackInt2();
+};
+
+
+#endif  // LIS3DSH_HPP_
---
+++ FreeRTOSProject/target/Src/config.h
@@ -0,0 +1,73 @@
+/**
+ * \file     config.h
+ *
+ * \licence "THE BEER-WARE LICENSE" (Revision 42):
+ *          <terry.louwers@fourtress.nl> wrote this file. As long as you retain
+ *          this notice you can do whatever you want with this stuff. If we
+ *          meet some day, and you think this stuff is worth it, you can buy me
+ *          a beer in return.
+ *                                                                Terry Louwers
+ *
+ * \brief   General configuration file for ExampleProject.
+ *
+ * \note    https://github.com/tlouwers/STM32F4-DISCOVERY/tree/develop/ExampleProject/target/Src
+ *
+ * \details Intended use is to provide an example how to configure the ASSERT
+ *          and EXPECT macros.
+ *
+ * \author  T. Louwers <terry.louwers@fourtress.nl>
+ * \version 1.0
+ * \date    05-2021
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+
+/************************************************************************/
+/* Includes                                                             */
+/************************************************************************/
+#include <stdint.h>
+#include <stddef.h>
+
+
+// Version
+static const char versionString[] = "My Embedded Project v0.1";
+
+
+// Available Expect/Assert modes
+#define HANDLE_BY_IGNORING     1
+#define HANDLE_BY_LOGGING      2
+#define HANDLE_BY_BREAKPOINT   3
+#define HANDLE_BY_RESETTING    4
+
+// Configuration of the EXPECT/ASSERT macros
+#define EXPECT_MODE            HANDLE_BY_LOGGING
+#define ASSERT_MODE            HANDLE_BY_RESETTING
+
+
+// Available LIS3DSH accelerometer settings
+#define REAL_LIS3DSH           1
+#define SIMULATED_LIS3DSH      2
+
+// Configuration of the LIS3DSH accelerometer
+#define LIS3DSH_ACCELEROMETER  REAL_LIS3DSH
+
+
+// Available simulated sensor output data
+#define SAWTOOTH_SIGNAL        1
+#define SEMI_REAL_SIGNAL       2
+
+// Configuration of the simulated sensor output data
+#define SIMULATED_SENSOR_OUTPUT_DATA    SAWTOOTH_SIGNAL
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // CONFIG_H_
