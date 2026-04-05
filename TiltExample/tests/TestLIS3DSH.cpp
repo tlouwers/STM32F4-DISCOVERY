5ff356828539bf0a193dc797f2af660f2a8032f5
Terry Louwers <terry.louwers@fourtress.nl>
2026-04-05 17:06:20 +0200

    Update CMSIS and HAL to v1.28.3 and fix TestLIS3DSH

--- TiltExample/tests/TestLIS3DSH.cpp
+++ TiltExample/tests/TestLIS3DSH.cpp
@@ -34,7 +34,8 @@ TEST_F(LIS3DSH_Test, Init_IsInit_Sleep)
 {
     EXPECT_FALSE(mSubject.IsInit());
 
-    EXPECT_TRUE(mSubject.Init(LIS3DSH::Config(LIS3DSH::SampleFrequency::_50_Hz,
+    EXPECT_TRUE(mSubject.Init(LIS3DSH::Config(false,
+                                              LIS3DSH::SampleFrequency::_50_Hz,
                                               LIS3DSH::Scale::_2_G,
                                               LIS3DSH::AntiAliasingFilter::_200_Hz)));
 
@@ -49,7 +50,8 @@ TEST_F(LIS3DSH_Test, Enable)
 {
     EXPECT_FALSE(mSubject.Enable());   // Not initialized yet
 
-    EXPECT_TRUE(mSubject.Init(LIS3DSH::Config(LIS3DSH::SampleFrequency::_50_Hz,
+    EXPECT_TRUE(mSubject.Init(LIS3DSH::Config(false,
+                                              LIS3DSH::SampleFrequency::_50_Hz,
                                               LIS3DSH::Scale::_2_G,
                                               LIS3DSH::AntiAliasingFilter::_200_Hz)));
 
@@ -60,7 +62,8 @@ TEST_F(LIS3DSH_Test, Disable)
 {
     EXPECT_FALSE(mSubject.Disable());   // Not initialized yet
 
-    EXPECT_TRUE(mSubject.Init(LIS3DSH::Config(LIS3DSH::SampleFrequency::_50_Hz,
+    EXPECT_TRUE(mSubject.Init(LIS3DSH::Config(false,
+                                              LIS3DSH::SampleFrequency::_50_Hz,
                                               LIS3DSH::Scale::_2_G,
                                               LIS3DSH::AntiAliasingFilter::_200_Hz)));
 
@@ -69,12 +72,13 @@ TEST_F(LIS3DSH_Test, Disable)
 
 TEST_F(LIS3DSH_Test, RetrieveAxesData)
 {
-    EXPECT_TRUE(mSubject.Init(LIS3DSH::Config(LIS3DSH::SampleFrequency::_50_Hz,
+    EXPECT_TRUE(mSubject.Init(LIS3DSH::Config(true,
+                                              LIS3DSH::SampleFrequency::_50_Hz,
                                               LIS3DSH::Scale::_2_G,
                                               LIS3DSH::AntiAliasingFilter::_200_Hz)));
 
     uint8_t motionArray[25 * 3 * 2] = {};       // 25 samples, X,Y,Z, 2 bytes/sample -- FIFO size
-    uint8_t motionLength = sizeof(motionArray); // Normally returned from interupt when data received
+    uint8_t motionLength = sizeof(motionArray); // Normally returned from interrupt when data received
 
     EXPECT_TRUE(mSubject.RetrieveAxesData(motionArray, motionLength));
 
